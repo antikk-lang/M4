63b10c35b2821a6943baa2b00321abbdcedda375
A a <antikk1231@gmail.com>
2026-03-22 15:22:20 +0500

    github

---
+++ header/main.js
@@ -0,0 +1,14 @@
+let burger = document.getElementById("burger")
+let main = document.getElementById("main")
+
+let active = false
+function activeMenu(){
+    active = !active
+    if (active){
+        menu.style.top = '100%'
+    }else {
+        menu.style.top = "-500%"
+    }
+}
+
+burger.addEventListener("click", activeMenu)
